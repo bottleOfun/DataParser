93ec34cd017a78996a5f6589e5e3a6deb1c2daf8
Ben <benjaminysdu@gmail.com>
2019-02-28 00:19:42 -0800

    add test of file reading to main method

--- src/main.java
+++ src/main.java
@@ -3,4 +3,10 @@
  * @author Benjamin Du
  */
 public class main {
+    public static void main(String[] args) {
+        // Test of utils
+
+        String data = Utils.readFileAsString("DataParser\\data/2016_Presidential_Results.csv");
+        System.out.println(data);
+    }
 }
